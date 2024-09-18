8e5bf0b3dab6c13f3f815b412bd5aef923a8d8b1
Victor Toupitzen Specian <victorts1991@gmail.com>
2024-09-18 11:34:03 -0300

    Update data-source-cli.ts

--- src/db/data-source-cli.ts
+++ src/db/data-source-cli.ts
@@ -6,7 +6,7 @@ const dataSourceOptions: DataSourceOptions = {
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD.toString(),
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   migrations: [__dirname + '/migrations/*.{js,ts}'],
